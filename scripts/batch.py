d89a0a86cfd4bb366f973e0a2b309e8a598b0884
Vladimir Ivannikov <vovannikov@yandex.ru>
2021-07-16 22:26:24 +0200

    More advanced match for all cases at once

--- scripts/batch.py
+++ scripts/batch.py
@@ -6,29 +6,36 @@ from os.path import isfile, join
 import csv
 import multiprocessing
 import time
+import argparse
+from pathlib import Path
 
-# 2D vs 3D simulations:
-is3D = True
-geoSym = "3" if is3D else "2"
-
-# What stiffness do we analyze
-#stiffnessMode = "tension"
-stiffnessMode = "torsion"
+# mesh template
+meshTpl = "universal"
 
 # Do real simulations of just debug
 doRealSimulations = True
 
-# paths
-pathParams = "/home/kapusta/development/spheres/settings/settings-tpl.prm"
-pathExec = "/home/kapusta/development/spheres-release/src/spheres_runner"
-pathGeo = "/home/kapusta/development/spheres/settings/spheres{}-{}-tpl.geo".format(geoSym, stiffnessMode)
-pathBatch = "/home/kapusta/work/batch/{}/{}d".format(stiffnessMode, geoSym)
+# paths TO DEFINE
+pathSpheresSrc = "~/development/spheres"
+pathExec = "~/development/spheres-release/src/spheres_runner"
+pathBatch = "~/work/spheres"
 
-pathMsh = pathBatch + "/mesh"
-pathLog = pathBatch + "/log"
-pathSet = pathBatch + "/prm"
+# Dependent paths
+pathParams = os.path.join(pathSpheresSrc, "settings/settings-tpl.prm")
+pathJobs = os.path.join(pathBatch, "jobs")
 
-def runCase(comb):
+def runCaseWrapper(data):
+    comb = data['comb']
+    options = data['options']
+    pathMsh = data['pathMsh']
+    pathLog = data['pathLog']
+    pathPrm = data['pathPrm']
+
+    runCase(comb, options, pathMsh, pathLog, pathPrm)
+
+def runCase(comb, options, pathMsh, pathLog, pathPrm):
+
+    geoSym = "3" if options['geometry'] == "3d" else "2"
 
     procId = multiprocessing.current_process()
     print("#{}: dA={}, dB={}, nratio={}, E={}".format(procId, comb['dA'], comb['dB'], comb['rNrat'], comb['E']))
@@ -48,14 +55,14 @@ def runCase(comb):
     caseFileName = "spheres{}_R1={:.1f}_R2={:.1f}_Rn={:.4f}_E={:.0f}".format(geoSym, R1, R2, Rn, comb['E'])
     fileMsh = "{}/{}.msh".format(pathMsh, caseFileName)
     fileLog = "{}/{}.log".format(pathLog, caseFileName)
-    fileSet = "{}/{}.prm".format(pathSet, caseFileName)
+    fileSet = "{}/{}.prm".format(pathPrm, caseFileName)
 
     # If log file exists, then we skip this case
-    if os.path.isfile(fileLog):
+    if os.path.isfile(fileLog) and not(options['force']):
         return
 
     # Generate mesh with gmsh
-    if doRealSimulations:
+    if doRealSimulations and (not(os.path.isfile(fileMsh)) or options['mesh']):
         cmdGmsh = "gmsh -{} {} -setnumber R1 {} -setnumber R2 {} -setnumber Rn {} -setnumber h1 {} -setnumber h2 {} -o {}".format(geoSym, pathGeo, R1, R2, Rn, strH1, strH2, fileMsh)
         stream = os.popen(cmdGmsh)
         output = stream.read()
@@ -68,10 +75,11 @@ def runCase(comb):
 
     caseParams = dataParamsTpl
     caseParams = caseParams.replace("%mesh%", fileMsh)
-    caseParams = caseParams.replace("%stiffness%", stiffnessMode)
+    caseParams = caseParams.replace("%stiffness%", options['stiffness'])
     caseParams = caseParams.replace("%x2%", str(offset))
     caseParams = caseParams.replace("%E%", str(E))
     caseParams = caseParams.replace("%log%", fileLog)
+    caseParams = caseParams.replace("%right%", str(options['right']))
 
     with open(fileSet, "w") as fileCaseSettings:
         fileCaseSettings.write(caseParams)
@@ -86,9 +94,37 @@ def runCase(comb):
         #os.system(cmdSpheres)
         #icomm = comm.Spawn(pathExec, args=fileSet, maxprocs=1, root=0)
 
-nproc = 6
+def runAllCases(options):
 
-if __name__ == '__main__':
+    # 2D vs 3D simulations:
+    geoSym = "3" if options['geometry'] == "3d" else "2"
+    secSym = "right" if options['right'] else "left"
+
+    # What stiffness do we analyze
+    #stiffnessMode = "tension"
+    #stiffnessMode = "torsion"
+    #stiffnessMode = "bending_displacement"
+    #stiffnessMode = "bending_rotation"
+    stiffnessMode = options['stiffness']
+
+    pathGeo = os.path.join(pathSpheresSrc, "settings/spheres{}-{}-tpl.geo".format(geoSym, meshTpl))
+    pathCase = os.path.join(pathJobs, "{}/{}d/{}".format(stiffnessMode, geoSym, secSym))
+
+    if not(meshTpl == stiffnessMode):
+        pathMsh = os.path.join(pathBatch, "mesh")
+    else:
+        pathMsh = os.path.join(pathCase, "mesh")
+
+    pathLog = os.path.join(pathCase, "log")
+    pathPrm = os.path.join(pathCase, "prm")
+
+    # Check and create directories
+    Path(pathCase).mkdir(parents=True, exist_ok=True)
+    Path(pathMsh).mkdir(parents=True, exist_ok=True)
+    Path(pathLog).mkdir(parents=True, exist_ok=True)
+    Path(pathPrm).mkdir(parents=True, exist_ok=True)
+
+    nproc = 6
 
     time_start = time.time()
 
@@ -98,9 +134,9 @@ if __name__ == '__main__':
     lstE = [5, 10, 20, 40, 70, 100, 130, 160, 200] # mkm, total = 9
 
     # Debug reduced list
-    #lstD = [38, 45]
-    #lstRnRatio = [0.01, 0.02]
-    #lstE = [5, 10]
+    lstD = [38, 45]
+    lstRnRatio = [0.01, 0.02]
+    lstE = [5, 10]
 
     nDiam = int(len(lstD) * (len(lstD) + 1) / 2)
     nRatio = len(lstRnRatio)
@@ -120,10 +156,16 @@ if __name__ == '__main__':
             if dA >= dB:
                 for rNratio in lstRnRatio:
                     for E in lstE:
-                        lstCombinations.append({'dA': dA, 'dB': dB, 'rNrat': rNratio, 'E': E})
+                        lstCombinations.append({
+                            'comb': {'dA': dA, 'dB': dB, 'rNrat': rNratio, 'E': E},
+                            'options': options,
+                            'pathMsh': pathMsh, 
+                            'pathLog': pathLog, 
+                            'pathPrm': pathPrm
+                        })
 
     with multiprocessing.Pool(processes=nproc) as pool:
-        pool.map(runCase, lstCombinations)
+        pool.map(runCaseWrapper, lstCombinations)
 
     time_end = time.time()
 
@@ -131,49 +173,94 @@ if __name__ == '__main__':
     minutes, seconds = divmod(rem, 60)
     print("Wall time: {:0>2}h:{:0>2}m:{:05.2f}s".format(int(hours),int(minutes),seconds))
 
-# Now merge all logs int a single data file
-logFiles = [f for f in listdir(pathLog) if isfile(join(pathLog, f))]
+    # Now merge all logs int a single data file
+    logFiles = [f for f in listdir(pathLog) if isfile(join(pathLog, f))]
+    logFiles.sort()
 
-csvHeader = ["R1", "R2", "Rn", "E", "Kzz"]
-lstRows = []
+    if stiffnessMode == "tension":
+        extraFields = ["Kuz"]
+    elif stiffnessMode == "torsion":
+        extraFields = ["Krz"]
+    elif stiffnessMode == "bending_displacement":
+        if options['right']:
+            extraFields = ["Kb13", "Kb23", "Kb33", "Kb43"]
+        else:
+            extraFields = ["Kb11", "Kb21", "Kb31", "Kb41"]
+    elif stiffnessMode == "bending_rotation":
+        if options['right']:
+            extraFields = ["Kb14", "Kb24", "Kb34", "Kb44"]
+        else:
+            extraFields = ["Kb12", "Kb22", "Kb32", "Kb42"]
 
-for fileCase in logFiles:
-    fileCaseFull = os.path.join(pathLog, fileCase)
+    csvMain = ["R1", "R2", "Rn", "E"]
+    csvHeader = csvMain + extraFields
+    lstRows = []
 
-    fileCaseNoExt = os.path.splitext(fileCase)[0]
-    fparts = fileCaseNoExt.split('_')
-    csvRow = [0] * (len(csvHeader))
+    for fileCase in logFiles:
+        fileCaseFull = os.path.join(pathLog, fileCase)
 
-    for part in fparts:
-        pair = part.split('=')
-        if len(pair) == 2:
-            pname = pair[0]
-            pvalue = pair[1]
-    
-            pid = csvHeader.index(pname)
-            csvRow[pid] = pvalue
+        fileCaseNoExt = os.path.splitext(fileCase)[0]
+        fparts = fileCaseNoExt.split('_')
+        csvRow = [0] * (len(csvMain))
 
-    with open(fileCaseFull, 'r') as f:
-        reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
+        for part in fparts:
+            pair = part.split('=')
+            if len(pair) == 2:
+                pname = pair[0]
+                pvalue = pair[1]
+        
+                pid = csvMain.index(pname)
+                csvRow[pid] = pvalue
 
-        stiffness = 0
-        for row in reader:
-            try:
+        with open(fileCaseFull, 'r') as f:
+            reader = csv.reader(f, delimiter=' ', skipinitialspace=True)
+
+            loadMagnitude = 1.0
+            csvStiffness = [0] * (len(extraFields))
+            for row in reader:
+                if row[0] == "time":
+                    continue
+                
                 t = float(row[0])
                 if t > 0.9:
-                    stiffness = float(row[1])
-            except ValueError:
-                stiffness = 0
+                    loadMagnitude = float(row[1])
+                    for i in range(len(extraFields)):
+                        try:
+                            csvStiffness[i] = float(row[2 + i]) / loadMagnitude
+                        except ValueError:
+                            csvStiffness[i] = 0
 
-        csvRow[-1] = stiffness
+            csvRow += csvStiffness
 
-    lstRows.append(csvRow)
+        lstRows.append(csvRow)
 
-# Write resultant CSV file
-csvStifFile = os.path.join(pathBatch, "{}_stiffness_{}d.csv".format(stiffnessMode, geoSym))
-with open(csvStifFile, 'w') as myfile:
-    wr = csv.writer(myfile, delimiter=',')
-    wr.writerow(csvHeader)
+    # Write resultant CSV file
+    csvStifFile = os.path.join(pathCase, "{}_stiffness_{}d.csv".format(stiffnessMode, geoSym))
+    with open(csvStifFile, 'w') as myfile:
+        wr = csv.writer(myfile, delimiter=',')
+        wr.writerow(csvHeader)
 
-    [wr.writerow(row) for row in lstRows]
+        [wr.writerow(row) for row in lstRows]
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument("-g", "--geometry", help="Geometry type", choices=['2d', '3d'], required=True)
+    parser.add_argument("-s", "--stiffness", help="Case to analyze", required=True,
+        choices=['tension', 'torsion', 'bending_displacement', 'bending_rotation'])
+    parser.add_argument("-m", "--mesh", help="Force mesh update", action='store_true', default=False)
+    parser.add_argument("-f", "--force", help="Force computations", action='store_true', default=False)
+    parser.add_argument("-r", "--right", help="Is right section active", action='store_true', default=True)
+
+    args = parser.parse_args()
+
+    options = {
+        'geometry': args.geometry,
+        'stiffness': args.stiffness,
+        'mesh': args.mesh,
+        'force': args.force,
+        'right': args.right,
+    }
+
+    runAllCases(options)
 
---
+++ scripts/run_all.py
@@ -0,0 +1,98 @@
+import os
+import numpy as np
+from batch import runAllCases
+from batch import pathJobs
+
+lstJobs = [
+    {'stiffness': 'tension', 'right': True},
+    {'stiffness': 'torsion', 'right': True},
+    {'stiffness': 'bending_displacement', 'right': False},
+    {'stiffness': 'bending_rotation', 'right': False},
+    {'stiffness': 'bending_displacement', 'right': True},
+    {'stiffness': 'bending_rotation', 'right': True}
+]
+
+geometry = '3d'
+doForceMesh = False
+doForceCalc = False
+
+for job in lstJobs:
+
+    options = {
+        'geometry': geometry,
+        'stiffness': job['stiffness'],
+        'mesh': doForceMesh,
+        'force': doForceCalc,
+        'right': job['right'],
+    }
+
+    print("Running job geometry={}, stiffness = {}, forceMesh = {}, forceCalc = {}, isRight = {}".format(geometry, job['stiffness'], doForceMesh, doForceCalc, job['right']))
+
+    runAllCases(options)
+
+
+# Now merge all csv together
+lstCsv = []
+for root, dirs, files in os.walk(pathJobs):
+    for file in files:
+        if file.endswith(".csv"):
+            filePath = os.path.join(root, file)
+            lstCsv.append(filePath)
+
+resPre = []
+resData = []
+isPreDone = False
+for csvFile in lstCsv:
+
+    resTable = np.genfromtxt(csvFile, delimiter=',', names=True)
+
+    for cname in resTable.dtype.names:
+        if cname.startswith('K'):
+            resData.append({'header': cname, 'data': resTable[cname]})
+        elif not(isPreDone):
+            resPre.append({'header': cname, 'data': resTable[cname]})
+
+    isPreDone = True
+
+def mycmp(item1, item2):
+    if item1['header'] < item2['header']:
+        return -1
+    elif item1['header'] > item2['header']:
+        return 1
+    else:
+        return 0
+
+def cmp_to_key(mycmp):
+    'Convert a cmp= function into a key= function'
+    class K(object):
+        def __init__(self, obj, *args):
+            self.obj = obj
+        def __lt__(self, other):
+            return mycmp(self.obj, other.obj) < 0
+        def __gt__(self, other):
+            return mycmp(self.obj, other.obj) > 0
+        def __eq__(self, other):
+            return mycmp(self.obj, other.obj) == 0
+        def __le__(self, other):
+            return mycmp(self.obj, other.obj) <= 0  
+        def __ge__(self, other):
+            return mycmp(self.obj, other.obj) >= 0
+        def __ne__(self, other):
+            return mycmp(self.obj, other.obj) != 0
+    return K
+
+resData.sort(key=cmp_to_key(mycmp))
+
+header = []
+csv = []
+
+for col in resPre:
+    header.append(col['header'])
+    csv.append(col['data'])
+
+for col in resData:
+    header.append(col['header'])
+    csv.append(col['data'])
+
+pathCsvResultant = os.path.join(pathJobs, 'stiffness_3d.csv')
+np.savetxt(pathCsvResultant, np.transpose(csv), header=','.join(header), comments='', delimiter=',')
